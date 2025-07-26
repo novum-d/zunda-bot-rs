d75b2d85cd88362e6b1c2e5baab1d3d4518dbc68
novum-d <hamada.tomoki01@gmail.com>
2025-07-27 00:50:10 +0900

    ♻️ refactor:  NOT NULL制約を追加

--- db/migrations/20250726085322_v1.0.0.up.sql
+++ db/migrations/20250726085322_v1.0.0.up.sql
@@ -3,15 +3,15 @@
 CREATE TABLE guild
 (
     guild_id VARCHAR(255) PRIMARY KEY,
-    name     VARCHAR(255)
+    name     VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE guild_member
 (
-    member_id  VARCHAR(255),
-    guild_id VARCHAR(255),
-    nickname VARCHAR(255),
-    birth    TIMESTAMP,
+    member_id VARCHAR(255),
+    guild_id  VARCHAR(255),
+    nickname  VARCHAR(255),
+    birth     TIMESTAMP NOT NULL,
     PRIMARY KEY (member_id, guild_id),
     FOREIGN KEY (guild_id) REFERENCES guild (guild_id)
 );
